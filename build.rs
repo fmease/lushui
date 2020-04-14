c780c9b745a52d58eaa7a0f02ebf79d60837672f
León Orell Valerian Liehr <liehr.exchange@gmx.net>
2020-04-15 01:34:44 +0200

    Fix build.rs

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() {
+    println!("cargo:rerun-if-changed=.git/index");
+
     let git_commit_hash = String::from_utf8(
         std::process::Command::new("git")
             .args(&["rev-parse", "HEAD"])
